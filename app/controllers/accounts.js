ebb0c2f95f00088d4f017319469761e9992dfc63
JohnDennehy <johndennehy101@gmail.com>
2021-02-17 22:52:54 +0000

    Initial skeleton of point of interest application

---
+++ app/controllers/accounts.js
@@ -0,0 +1,99 @@
+'use strict'
+const User = require('../models/user');
+
+const Accounts = {
+index: {
+  auth: false,
+  handler: function(request, h) {
+    return h.view("main", {title: "Welcome to Donations"})
+  }
+},
+  showSignup: {
+  auth: false,
+  handler: function(request, h) {
+    return h.view("signup", {title: "Sign up for donations"})
+  }
+  },
+  signup: {
+    auth: false,
+    handler: async function(request, h) {
+      const { email } = request.payload;
+      try {
+        let checkEmailInUse = await User.findByEmail(email);
+        if (checkEmailInUse) {
+          const message = "Email address already in use";
+          throw Boom.unauthorized(message);
+        }
+
+
+        const payload = request.payload;
+        const newUser = new User({
+          firstName: payload.firstName,
+          lastName: payload.lastName,
+          email: payload.email,
+          password: payload.password
+        });
+        const user = await newUser.save();
+        request.cookieAuth.set({ id: user.id });
+        return h.redirect("/home");
+      } catch (err) {
+        return h.view("signup", { errors: [{ message: err.message }] });
+      }
+
+    }
+  },
+  showSettings: {
+    handler: async function(request, h) {
+      const id = request.auth.credentials.id;
+      const user = await User.findById(id).lean();
+      return h.view('settings', { title: 'Donation Settings', user: user });
+    }
+  },
+  updateSettings: {
+    handler: async function(request, h) {
+      const userEdit = request.payload;
+      const id = request.auth.credentials.id;
+      const user = await User.findById(id);
+      user.firstName = userEdit.firstName;
+      user.lastName = userEdit.lastName;
+      user.email = userEdit.email;
+      user.password = userEdit.password;
+      await user.save();
+      return h.redirect("/settings");
+    }
+  },
+  showLogin: {
+  auth: false,
+  handler: function(request, h) {
+return h.view("login", {title: "Login to Donations"})
+  }
+  },
+  login: {
+    auth: false,
+    handler: async function(request, h) {
+      const { email, password } = request.payload;
+      try {
+        let user = await User.findByEmail(email);
+        if (!user) {
+          const message = "Email address is not registered";
+          throw Boom.unauthorized(message);
+        }
+        user.comparePassword(password);
+        request.cookieAuth.set({ id: user.id });
+        return h.redirect("/home");
+      } catch (err) {
+        return h.view("login", { errors: [{ message: err.message }] });
+      }
+    }
+  },
+  logout: {
+    handler: function (request, h) {
+      request.cookieAuth.clear();
+      return h.redirect("/");
+    },
+  },
+
+
+}
+
+module.exports = Accounts;
